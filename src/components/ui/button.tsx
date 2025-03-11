61f4dd79dabaa43f05acfe8e428efd52b7c55c77
eunbime <ebk1014@gmail.com>
2025-03-11 17:31:26 +0900

    [#23]feature: Button 공통 컴포넌트 구현

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,11 +4,18 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+const baseStyle = {
+  rounded: "rounded-xl",
+  text: "text-base font-semibold",
+  hover: "hover:bg-gray-100",
+};
+
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "text-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
+        // Base variants
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
@@ -19,6 +26,54 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+
+        // Purple variants
+        purple: cn(
+          "bg-purple-600 text-white hover:bg-purple-700 ",
+          baseStyle.rounded,
+          baseStyle.text,
+        ),
+        "purple-secondary": cn(
+          "bg-purple-700 text-white hover:bg-purple-800 ",
+          baseStyle.rounded,
+          baseStyle.text,
+        ),
+        "purple-tertiary": cn(
+          "bg-purple-800 text-white hover:bg-purple-900 ",
+          baseStyle.rounded,
+          baseStyle.text,
+        ),
+        "purple-outline": cn(
+          "border border-purple-600 text-purple-600 hover:bg-purple-600 ",
+          baseStyle.rounded,
+          baseStyle.text,
+          baseStyle.hover,
+        ),
+        "purple-outline-secondary": cn(
+          "border border-purple-500 text-purple-500 hover:bg-purple-500 ",
+          baseStyle.rounded,
+          baseStyle.text,
+          baseStyle.hover,
+        ),
+        "purple-outline-tertiary": cn(
+          "border border-purple-700 text-purple-700 hover:bg-purple-700 ",
+          baseStyle.rounded,
+          baseStyle.text,
+          baseStyle.hover,
+        ),
+
+        // Gray variants
+        gray: cn(
+          "bg-gray-400 text-white hover:bg-gray-500 ",
+          baseStyle.rounded,
+          baseStyle.text,
+        ),
+        "gray-outline": cn(
+          "border border-gray-400 text-gray-400 hover:bg-gray-400 ",
+          baseStyle.rounded,
+          baseStyle.text,
+          baseStyle.hover,
+        ),
       },
       size: {
         default: "h-9 px-4 py-2",
