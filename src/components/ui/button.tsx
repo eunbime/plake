1188a6042b0a216d78a2053d9e56de44f0665853
eunbime <ebk1014@gmail.com>
2025-03-11 18:19:08 +0900

    [#23]feature: Button 컴포넌트 테스트 코드 작성

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -93,15 +93,31 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  "aria-label"?: string;
+  "aria-disabled"?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      "aria-label": ariaLabel,
+      "aria-disabled": ariaDisabled,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        role="button"
+        aria-label={ariaLabel}
+        aria-disabled={ariaDisabled}
         {...props}
       />
     );
