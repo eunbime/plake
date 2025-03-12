3da33a03d51f633d0cac44a2e31a36cba983f838
eunbime <ebk1014@gmail.com>
2025-03-12 12:21:25 +0900

    [#30]design: 모임 상세 페이지 UI 구현 - 모임 상세 페이지 컴포넌트 추가 및 UI 구현 - 공통 컴포넌트 디테일 수정

--- src/components/common/DateTimeTag.tsx
+++ src/components/common/DateTimeTag.tsx
@@ -2,30 +2,42 @@ import clsx from "clsx";
 import dayjs from "dayjs";
 
 interface IDateTimeTagProps {
-  type: "date" | "time";
   size?: "small" | "medium";
-  date?: Date;
+  date: Date;
 }
 
-const DateTimeTag = ({
-  type = "date",
-  size = "medium",
-  date,
-}: IDateTimeTagProps) => {
+const baseStyle =
+  "flex items-center justify-center rounded-[4px] bg-purple-200 py-[2px] text-sm font-medium";
+
+const DateTimeTag = ({ size = "medium", date }: IDateTimeTagProps) => {
   const currentDate = dayjs(date);
   const dateText = currentDate.format("M월 DD일");
   const timeText = currentDate.format("HH:mm");
 
   return (
-    <div
-      className={clsx(
-        "flex items-center justify-center rounded-[4px] bg-purple-200 py-[2px] text-sm font-medium",
-        type === "date" ? "text-black" : "text-purple-600",
-        size === "small" && "px-[6px] text-xs",
-        size === "medium" && "px-2 text-sm",
-      )}
-    >
-      {type === "date" ? dateText : timeText}
+    <div className="flex gap-2" role="group" aria-label="날짜 및 시간">
+      <time
+        aria-label="날짜"
+        className={clsx(
+          "text-black",
+          baseStyle,
+          size === "small" && "px-[6px] text-xs",
+          size === "medium" && "px-2 text-sm",
+        )}
+      >
+        {dateText}
+      </time>
+      <time
+        aria-label="시간"
+        className={clsx(
+          "text-purple-600",
+          baseStyle,
+          size === "small" && "px-[6px] text-xs",
+          size === "medium" && "px-2 text-sm",
+        )}
+      >
+        {timeText}
+      </time>
     </div>
   );
 };
---
+++ src/components/gathering-detail/GathetingImage.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const GatheringImage = () => {
+  return (
+    <figure className="relative min-h-[270px] flex-1 overflow-hidden rounded-3xl bg-gray-300">
+      <Image src="" alt="gathering-image" fill className="object-cover" />
+    </figure>
+  );
+};
+
+export default GatheringImage;
