53311599158bab97be61051794f812f2b107dcb4
k-impossible <kth3349@gmail.com>
2025-03-12 13:49:42 +0900

    [#28]feature: Avatar 컴포넌트 구현, favicon 추가

---
+++ src/components/common/Avatar.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+type TAvatarProps = {
+  type: "default" | "editable";
+  size: "small" | "default" | "large";
+  imgPath?: string;
+  handleAvatar: () => void;
+};
+const AVATAR_SIZE = {
+  small: 25,
+  default: 35,
+  large: 55,
+};
+const Avatar = ({
+  type,
+  size,
+  imgPath = "default",
+  handleAvatar,
+}: TAvatarProps) => {
+  const selectedSize = AVATAR_SIZE[size];
+
+  const avatarImg = () => {
+    if (imgPath === "default") {
+      return `/images/avatar_${type}.png`;
+    }
+    return imgPath;
+  };
+
+  return (
+    <div className="overflow-hidden rounded-full">
+      <Image
+        className="cursor-pointer"
+        onClick={handleAvatar}
+        src={avatarImg()}
+        alt={`avatar-${type}`}
+        aria-label={`avatar-${type}`}
+        width={selectedSize}
+        height={selectedSize}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
