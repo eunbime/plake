2f0ab973856d2bb2f08a6c53b8e843c6ebd491a5
eunbime <ebk1014@gmail.com>
2025-03-13 18:11:01 +0900

    [#30]chore: 병합

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -10,8 +10,6 @@ const Sheet = SheetPrimitive.Root;
 
 const SheetTrigger = SheetPrimitive.Trigger;
 
-// const SheetClose = SheetPrimitive.Close;
-
 const SheetPortal = SheetPrimitive.Portal;
 
 const SheetOverlay = React.forwardRef<
@@ -63,10 +61,6 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      {/* <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close> */}
       {children}
     </SheetPrimitive.Content>
   </SheetPortal>
