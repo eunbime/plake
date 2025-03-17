b0fb3cd88b49bfa858a0bda4015084b99a89c1ec
eunbime <ebk1014@gmail.com>
2025-03-17 14:54:53 +0900

    [#51]feature: createPortal 적용

---
+++ src/components/modals/confirm-alert-modal/ConfirmAlertModal.tsx
@@ -0,0 +1,11 @@
+import Modal from "../Modal";
+
+const ConfirmAlertModal = () => {
+  return (
+    <Modal isOpen={true} onClose={() => {}}>
+      <div>ConfirmAlertModal</div>
+    </Modal>
+  );
+};
+
+export default ConfirmAlertModal;
