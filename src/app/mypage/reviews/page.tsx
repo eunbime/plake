13dbc6a353ab0b29cfbc55e85e3791a0cf738610
yunhayeon <yhy5049@naver.com>
2025-03-25 18:56:26 +0900

    [#67]feature: 나의 리뷰 탭 작업 진행 중

--- src/app/mypage/reviews/page.tsx
+++ src/app/mypage/reviews/page.tsx
@@ -1,18 +1,41 @@
 import { Suspense } from "react";
 
-import ReviewContent from "@/app/mypage/_components/ReviewContent";
+import MyCardList from "@/app/mypage/_components/MyCardList";
 import ReviewTab from "@/app/mypage/_components/ReviewTab";
+import FetchBoundary from "@/components/boundary/FetchBoundary";
+import LoadingDots from "@/components/common/LoadingDots";
+import { getCookieOfToken } from "@/utils/cookieToken";
 
-const Page = () => {
+interface PageProps {
+  searchParams: {
+    type?: string;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const token = await getCookieOfToken();
+
+  if (!token) return null;
+
+  const isRoot = !searchParams.type;
+
   return (
     <>
       <Suspense>
         <ReviewTab />
       </Suspense>
 
-      <Suspense>
-        <ReviewContent />
-      </Suspense>
+      {isRoot ? (
+        <FetchBoundary fallback={<LoadingDots />}>
+          <MyCardList
+            direction="reviews"
+            token={token}
+            emptyMessage="아직 작성 가능한 리뷰가 없어요"
+          />
+        </FetchBoundary>
+      ) : (
+        <div className="p-4 text-lg font-semibold">작성 예정</div>
+      )}
     </>
   );
 };
