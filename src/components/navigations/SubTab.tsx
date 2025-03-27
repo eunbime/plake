24544ac7c76ed96c8db1d53d224e94e41a7da119
write-code-b <minjichoi.official@gmail.com>
2025-03-28 08:31:43 +0900

    [#72]fix: subtab href 버그 수정

--- src/components/navigations/SubTab.tsx
+++ src/components/navigations/SubTab.tsx
@@ -14,27 +14,28 @@ const SubTab = () => {
   const { searchParams, setSearchParams } = useCustomSearchParams();
 
   const type = searchParams.get("type") || "";
+  const isOffline = pathname === OFFLINE_PATH;
+
+  const href = pathname === OFFLINE_PATH ? OFFLINE_PATH : ONLINE_PATH;
 
   return (
     <div className="align-center flex gap-2">
-      {SUB_TAB[pathname === OFFLINE_PATH ? "OFFLINE" : "ONLINE"].map(
-        (tab, i) => (
-          <Link href={tab.value ? `?type=${tab.value}` : ONLINE_PATH} key={i}>
-            <Button
-              variant="default"
-              aria-label="서브 주제 탭"
-              onClick={() => setSearchParams({ type: tab.value })}
-              className={cn(
-                "rounded-xl px-4 py-2.5",
-                tab.value !== type &&
-                  "bg-gray-200 text-black hover:bg-gray-200/90",
-              )}
-            >
-              {tab.name}
-            </Button>
-          </Link>
-        ),
-      )}
+      {SUB_TAB[isOffline ? "OFFLINE" : "ONLINE"].map((tab, i) => (
+        <Link href={tab.value ? `?type=${tab.value}` : href} key={i}>
+          <Button
+            variant="default"
+            aria-label="서브 주제 탭"
+            onClick={() => setSearchParams({ type: tab.value })}
+            className={cn(
+              "rounded-xl px-4 py-2.5",
+              tab.value !== type &&
+                "bg-gray-200 text-black hover:bg-gray-200/90",
+            )}
+          >
+            {tab.name}
+          </Button>
+        </Link>
+      ))}
     </div>
   );
 };
