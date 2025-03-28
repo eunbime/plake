008b9a71f87670c805dfc4fd550bf8ec2175d978
eunbime <ebk1014@gmail.com>
2025-03-28 10:13:55 +0900

    [#30]design: 리뷰목록 css 수정

--- src/app/gathering/detail/[id]/_components/GatheringReviewList.tsx
+++ src/app/gathering/detail/[id]/_components/GatheringReviewList.tsx
@@ -28,30 +28,32 @@ const GatheringReviewList = () => {
   };
 
   return (
-    <section className="flex h-full min-h-[80vh] flex-col gap-6 border-t-2 border-gray-200 bg-white p-6 pb-20">
-      <p className="text-lg font-semibold">
-        {"이용자들은 이 프로그램을 이렇게 느꼈어요!"}
-      </p>
-      {data.pages[0].data.length === 0 ? (
-        <div className="flex h-full items-center justify-center">
-          <p className="text-center text-sm text-gray-500">
-            {"아직 리뷰가 없어요."}
-          </p>
-        </div>
-      ) : (
-        <div className="flex h-full flex-col justify-between">
-          {data.pages
-            .flatMap(page => page.data)
-            .map(review => (
-              <GatheringReviewItem key={review.id} review={review} />
-            ))}
-          <Pagination
-            totalPage={data.pages[0].totalPages}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-          />
-        </div>
-      )}
+    <section className="flex h-full min-h-[100vh] flex-col justify-between gap-10 border-t-2 border-gray-200 bg-white p-6 pb-20">
+      <div className="flex h-full flex-col gap-6">
+        <p className="text-lg font-semibold">
+          {"이용자들은 이 프로그램을 이렇게 느꼈어요!"}
+        </p>
+        {data.pages[0].data.length === 0 ? (
+          <div className="flex h-full items-center justify-center">
+            <p className="text-center text-sm text-gray-500">
+              {"아직 리뷰가 없어요."}
+            </p>
+          </div>
+        ) : (
+          <>
+            {data.pages
+              .flatMap(page => page.data)
+              .map(review => (
+                <GatheringReviewItem key={review.id} review={review} />
+              ))}
+          </>
+        )}
+      </div>
+      <Pagination
+        totalPage={data.pages[0].totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </section>
   );
 };
