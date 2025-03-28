c08632e12f6edba9c7883497fb70da492d35301a
eunbime <ebk1014@gmail.com>
2025-03-28 13:24:06 +0900

    [#30]fix: TimePicker 수정

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -9,6 +9,7 @@ interface IUseCalendar {
 }
 
 export const useCalendar = (props?: IUseCalendar) => {
+  console.log(props?.disabledAfterDate);
   const defaultDate = props?.defaultDate ?? new Date();
   const disabledBeforeDate = new Date();
 
@@ -24,9 +25,18 @@ export const useCalendar = (props?: IUseCalendar) => {
     selected: selectedDate,
     onSelect: handleDateChange,
     defaultMonth: selectedDate,
-    disabled: props?.disabledAfterDate
-      ? { before: disabledBeforeDate, after: props.disabledAfterDate }
-      : { before: disabledBeforeDate },
+    fromDate: props?.disabledAfterDate
+      ? disabledBeforeDate.toDateString() ===
+        props.disabledAfterDate.toDateString()
+        ? new Date(9999, 0, 1) // 모든 날짜를 비활성화
+        : disabledBeforeDate
+      : disabledBeforeDate,
+    toDate: props?.disabledAfterDate
+      ? disabledBeforeDate.toDateString() ===
+        props.disabledAfterDate.toDateString()
+        ? new Date(0) // 모든 날짜를 비활성화
+        : props.disabledAfterDate
+      : undefined,
     locale: ko,
   };
 
