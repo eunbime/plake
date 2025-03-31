3bfe3bd39e6c1d11bdcc6402ac7122351388ea39
write-code-b <minjichoi.official@gmail.com>
2025-03-31 09:18:01 +0900

    [#102]feature: ids 기준 찜한 모임 데이터 패칭

---
+++ src/app/favorites/_components/FavoriteSubTab.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/Button";
+import { SUB_TAB } from "@/constants/ui";
+import { cn } from "@/lib/utils";
+import useTabStore from "@/stores/useTabStore";
+
+const GatheringSubTab = () => {
+  const tabIdxs = useTabStore(state => state.tabIdxs);
+  const setTabIdxs = useTabStore(state => state.setTabIdxs);
+
+  const mainTabIdx = tabIdxs[0];
+  const subTabIdx = tabIdxs[1];
+
+  return (
+    <div className="flex items-center gap-2">
+      {SUB_TAB[mainTabIdx === 0 ? "OFFLINE" : "ONLINE"].map((tab, i) => (
+        <Button
+          key={i}
+          variant="default"
+          aria-label="서브 주제 탭"
+          className={cn(
+            "rounded-xl px-4 py-2.5",
+            subTabIdx !== i && "bg-gray-200 text-black hover:bg-gray-200/90",
+          )}
+          onClick={() => setTabIdxs([mainTabIdx, i])}
+        >
+          {tab.name}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default GatheringSubTab;
--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,23 @@
-import GatheringFavoriteList from "./_components/GatheringFavoriteList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+import FetchBoundary from "@/components/boundary/FetchBoundary";
+import MainCardListSkeleton from "@/components/skeletons/MainCardListSkeleton";
+
+import FavoriteList from "./_components/FavoriteList";
 
 const Page = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="base-wrap">
-      <GatheringFavoriteList />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <FetchBoundary fallback={<MainCardListSkeleton />}>
+        <FavoriteList />
+      </FetchBoundary>
+    </HydrationBoundary>
   );
 };
 
