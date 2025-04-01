32abcd7860cec0768afa23539dcc8f7cc65a9da4
eunbime <ebk1014@gmail.com>
2025-04-01 15:26:39 +0900

    [#30]design: 세부적인 UI 수정

--- src/app/(home)/_components/MainPromotionContainer.tsx
+++ src/app/(home)/_components/MainPromotionContainer.tsx
@@ -2,7 +2,7 @@ import MainPromotion from "./MainPromotion";
 
 const MainPromotionContainer = () => {
   return (
-    <section className="flex flex-col gap-20">
+    <section className="flex flex-col gap-40 md:gap-20">
       <MainPromotion type="offline" />
       <MainPromotion type="online" />
     </section>
