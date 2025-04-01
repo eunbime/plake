a53139217f2064b9a9b214fe4cc6cfe9d05fb9fc
write-code-b <minjichoi.official@gmail.com>
2025-04-01 20:45:23 +0900

    [#102]fix: 모임 찜하기 기능 개선

--- src/app/favorites/_components/FavoriteList.tsx
+++ src/app/favorites/_components/FavoriteList.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useShallow } from "zustand/shallow";
 
 import MainCardItem from "@/components/layout/MainCardItem";
-import { favoriteFilterKey, IFavorite } from "@/constants/favorite";
+import { favoriteFilterKey, IfavoriteAll } from "@/constants/favorite";
 import { useSuspenseFavoriteList } from "@/hooks/gathering/useFavoriteList";
 import useFavoriteStore from "@/stores/useFavoriteStore";
 import useTabStore from "@/stores/useTabStore";
@@ -17,8 +17,9 @@ const FavoriteList = () => {
   const { favorite } = useFavoriteStore(
     useShallow(state => ({ favorite: state.favorite })),
   );
+  const favoriteList = useFavoriteStore(state => state.favoriteList);
+  const setFavoriteList = useFavoriteStore(state => state.setFavoriteList);
 
-  const [favoriteList, setFavoriteList] = useState<string[]>([]);
   const [filter, setFilter] = useState<string>("");
 
   const email = user?.email || "unknown";
@@ -28,16 +29,17 @@ const FavoriteList = () => {
   });
 
   useEffect(() => {
-    const data: IFavorite = Object.assign({ default: [] }, favorite);
+    if (typeof window !== "undefined") {
+      const data: IfavoriteAll = Object.assign(favorite);
+      const idsByuser = data?.favoriteAll?.[email];
 
-    const idsByUser = data?.[email];
-    if (idsByUser) setFavoriteList(idsByUser);
-
-    setFilter(favoriteFilterKey[tabIdxs.join("-")]);
-  }, [tabIdxs, email, favorite]);
+      setFavoriteList(idsByuser);
+      setFilter(favoriteFilterKey[tabIdxs.join("-")]);
+    }
+  }, [email, favorite, setFavoriteList, tabIdxs]);
 
   useEffect(() => {
-    setFilter("OFFLINE");
+    useFavoriteStore.persist.rehydrate(); //Localstorage에서 favorite key의 최신값을 가져온다.
   }, []);
 
   return (
