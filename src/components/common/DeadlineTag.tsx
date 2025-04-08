f2570599ceaf2b1caf01dbfc1d17f2e5f916179e
write-code-b <minjichoi.official@gmail.com>
2025-04-08 19:06:19 +0900

    [#143]test: DeadlineTag 테스트 리팩토링

--- src/components/common/DeadlineTag.tsx
+++ src/components/common/DeadlineTag.tsx
@@ -1,19 +1,18 @@
 import dayjs from "dayjs";
 import { FaClock } from "react-icons/fa6";
 
-interface IDeadlineTagProps {
+interface IDeadlineTag {
   registrationEnd: dayjs.Dayjs;
 }
 
-const DeadlineTag = ({ registrationEnd }: IDeadlineTagProps) => {
+const DeadlineTag = ({ registrationEnd }: IDeadlineTag) => {
   const today = dayjs().format("YYYY-MM-DDTHH:mm:ss"); //오늘일자
+  const deadlineHour = registrationEnd.format("H");
 
-  const deadline = dayjs(registrationEnd); //마감일자
-  const deadlineHour = deadline.format("H");
-
-  const isOpened = deadline.isAfter(today); // 모임오픈 여부 (모임이 종료되지 않았을 경우: true)
+  const isOpened = registrationEnd.isAfter(today); // 모임오픈 여부 (모임이 종료되지 않았을 경우: true)
   const isEndDay =
-    dayjs().format("YYYY/MM/DD") === deadline.format("YYYY/MM/DD"); //오늘일자와 마감일자가 같으면 종료일로 설정
+    dayjs().format("YYYY/MM/DD") === registrationEnd.format("YYYY/MM/DD"); //오늘일자와 마감일자가 같으면 종료일로 설정
+  const diff = registrationEnd.diff(today, "day") || "1";
 
   return (
     <>
@@ -24,9 +23,7 @@ const DeadlineTag = ({ registrationEnd }: IDeadlineTagProps) => {
         >
           <FaClock size={14} />
           <span className="text-xs">
-            {isEndDay
-              ? `오늘 ${deadlineHour}시 마감`
-              : `${deadline.diff(today, "day")}일 후 마감`}
+            {isEndDay ? `오늘 ${deadlineHour}시 마감` : `${diff}일 후 마감`}
           </span>
         </section>
       )}
